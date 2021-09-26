2e672b3b60eb12627ef826035b5314661d7494f5
vnivy <nivethavenkatesh2001.com>
2021-09-26 15:02:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min';
+import Homechef from './Components/Homechef.js/Homechef';
+
+function App() {
+  return (
+    <div className="App">
+      <Homechef></Homechef>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Homechef.js/Homechef.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Homechef() {
+    return (
+        <div>
+            <h1 className="text-danger">hi</h1>
+        </div>
+    );
+}
+export default Homechef;
